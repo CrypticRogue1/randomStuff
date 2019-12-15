4fb41546aa32435f308f4d6afc8ad2929318c0e8
Cryptic_Rogue1 <53357237+CrypticRogue1@users.noreply.github.com>
2019-12-15 14:20:47 -0600

    Testing Function
    Wanted to get a To Do list set up and see how to structure a function correctly before I start to make the program itself. I'll add more as I go on.
    
    Maybe the drawing will be hard but it'll be ok. :+1:

--- vendingMachine.cpp
+++ vendingMachine.cpp
@@ -1,17 +1,28 @@
-#include <iostream>
-#include <string>
-
-using namespace std;
-int main(){
 /*
 This program is a vending machine
 Note: This is a program to practice functions. A function is something where you have the actual thing 
 that it does somewhere else on the screen. This can be an output or a calculation or algorithm.
 To do:
+ - Output their change
+ - Output items, as a picture if possible.
+ - Make a bill insert limit if things being bought isn't >thing going to buy. 
+ - Display initial output with a visual vending machine
+    ~ This will contain some chips, cookies, gummy bears, chocolate, gum, etc.
 */
+#include <iostream>
+#include <string>
 
+using namespace std;
+int main(){
 
+void vendingMachine();
 
+vendingMachine();
+cout << "*This is after.*";
 
 return 0;
+}
+
+void vendingMachine(){
+    cout << "This is the vendingMachine function output. " << endl;
 }
