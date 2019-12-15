e4f70cc0e4cafcd5f54dfe902ec216ad81e51c9b
Cryptic_Rogue1 <53357237+CrypticRogue1@users.noreply.github.com>
2019-12-15 13:17:08 -0600

    Create vendingMachine.cpp

---
+++ vendingMachine.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main(){
+/*
+This program is a vending machine
+Note: This is a program to practice functions. A function is something where you have the actual thing 
+that it does somewhere else on the screen. This can be an output or a calculation or algorithm.
+To do:
+*/
+
+
+
+
+return 0;
+}
