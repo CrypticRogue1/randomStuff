13c81e8ee91ad2b14c5d1218d111819296d30748
Cryptic_Rogue1 <53357237+CrypticRogue1@users.noreply.github.com>
2019-12-19 14:17:06 -0600

    Without functions
    So far it's looking alrighty.

--- vendingMachine.cpp
+++ vendingMachine.cpp
@@ -11,18 +11,50 @@ To do:
 */
 #include <iostream>
 #include <string>
-
 using namespace std;
+
 int main(){
 
-void vendingMachine();
+    float change = 0.00, moneyIn = 0.00, charging = 0.00;
+    int const ITEMS = 3;
+    string vendingItems[ITEMS] = {"Ice_cream", "Cake", "Cookies"};
+    string vendingNumbers[ITEMS];
+    string helpAnswer;
+    string choice;
+    int items;
+    
+    cout << "Hello, do you need some help? Type 'yes' of 'no'" << endl;
+    cin >> helpAnswer;
+/* This is what I'm thnking for lines 29 through 38: 
+                Everything looks ok but the 'vendingNumbers[items + 1]' part.
+                I think this could do it the way I inted for it to do it which is 1, 2, 3,
+                The other bad way is that it does it but it only counts like two numbers maybe. idk*/
+ 
+    while(helpAnswer == "yes"){
 
-vendingMachine();
-cout << "*This is after.*";
+        cout << "These are the things I have: " << endl;
+        
+        for(items = 0; vendingItems[items] < vendingItems[ITEMS]; items++){
+            cout << vendingNumbers[items + 1] << ". " << vendingItems[items] << endl;
+        }
 
-return 0;
-}
+        cout << "Which one would you like?" << endl << "Choose number "; // << endl;
+        
+        for(items = 0; vendingNumbers[items] < vendingNumbers[ITEMS]; items++){
+            cout << vendingNumbers[items + 1] << ", ";
+        }
 
-void vendingMachine(){
-    cout << "This is the vendingMachine function output. " << endl;
+        cin >> choice;
+
+    }
+    if(helpAnswer != "yes" && helpAnswer != "no"){
+        cout << "Please enter, 'yes' of 'no' ";
+        cin >> helpAnswer;
+    }
+    
+
+
+
+
+    return 0;
 }
