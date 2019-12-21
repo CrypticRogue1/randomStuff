ae76f9ae83385ab72c78e50feba909a8276e4741
Cryptic_Rogue1 <53357237+CrypticRogue1@users.noreply.github.com>
2019-12-20 20:19:07 -0600

    I'll download an actual compiler
    I'll download it to see which lines or whatever it is that it's- might not be working on lines 45-47.

--- vendingMachine.cpp
+++ vendingMachine.cpp
@@ -15,9 +15,10 @@ using namespace std;
 
 int main(){
 
-    float change = 0.00, moneyIn = 0.00, charging = 0.00;
+    double change = 0.00, moneyIn = 0.00, charging = 0.00;
     int const ITEMS = 3;
     string vendingItems[ITEMS] = {"Ice_cream", "Cake", "Cookies"};
+    double vendingPrices[ITEMS] = {00.50, 2.00, 1.50};
     string vendingNumbers[ITEMS];
     string helpAnswer;
     string choice;
@@ -29,27 +30,89 @@ int main(){
                 Everything looks ok but the 'vendingNumbers[items + 1]' part.
                 I think this could do it the way I inted for it to do it which is 1, 2, 3,
                 The other bad way is that it does it but it only counts like two numbers maybe. idk*/
- 
+
+
+//
+//
+//
+//
+//
     while(helpAnswer == "yes"){
 
         cout << "These are the things I have: " << endl;
-        
+            // So I've seen that this might loop forever (?) I feel like I need an actual compiler on this so I can test it better. 
+            // I'll have to download the visual studio too.
         for(items = 0; vendingItems[items] < vendingItems[ITEMS]; items++){
-            cout << vendingNumbers[items + 1] << ". " << vendingItems[items] << endl;
+            cout << vendingNumbers[items + 1] << ". " << vendingItems[items] << "$" << vendingPrices[items] << endl;
         }
 
-        cout << "Which one would you like?" << endl << "Choose number "; // << endl;
-        
+        cout << "Which one would you like?" << endl << "Choose number " << endl;
+            
         for(items = 0; vendingNumbers[items] < vendingNumbers[ITEMS]; items++){
-            cout << vendingNumbers[items + 1] << ", ";
+            cout << vendingNumbers[items + 1] << "  ";
         }
 
         cin >> choice;
+            
+        for(items = 0; choice != vendingNumbers[items]; items++){
+            if(choice == vendingNumbers[items]){
+                cout << "You have chosen: " << vendingNumbers[items] << " " << vendingItems[items] << ", " << "$" << vendingPrices[items] << endl; 
+                //Need to add the asking for the money next
+            }
+        }
+
+
+        cout << "Do you need any more help? ";
+        cin >> helpAnswer;
+
+        if(helpAnswer != "yes" && helpAnswer != "no"){
+            cout << "Please enter 'yes' of 'no': ";
+            cin >> helpAnswer;
+        }
+    }
+    cout << "This is the EOP"; // This goes off if it ends up being a no.
+//
+//
+//
+//
+//
+//
+    if(helpAnswer == "yes"){
 
     }
-    if(helpAnswer != "yes" && helpAnswer != "no"){
-        cout << "Please enter, 'yes' of 'no' ";
-        cin >> helpAnswer;
+    else if(helpAnswer != "yes" && helpAnswer != "no"){
+
+    }
+    else{
+
+    }
+
+    while(helpAnswer != "no"){
+
+        while(helpAnswer == "yes"){
+
+            cout << "These are the things I have: " << endl;
+            
+            for(items = 0; vendingItems[items] < vendingItems[ITEMS]; items++){
+                cout << vendingNumbers[items + 1] << ". " << vendingItems[items] << "$" << vendingPrices[items] << endl;
+            }
+
+            cout << "Which one would you like?" << endl << "Choose number " << endl;
+            
+            for(items = 0; vendingNumbers[items] < vendingNumbers[ITEMS]; items++){
+                cout << vendingNumbers[items + 1] << "  ";
+            }
+
+            cin >> choice;
+            
+            for(items = 0; choice != vendingNumbers[items]; items++){
+                if(choice == vendingNumbers[items]){
+                    cout << "You have chosen: " << vendingNumbers[items] << " " << vendingItems[items] << ", " << "$" << vendingPrices[items] << endl; 
+                    //Need to add the asking for the money next
+                }
+            }
+
+        }
     }
     
 
